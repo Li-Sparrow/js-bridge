d2adfbe1a28cd28541d77ddbd0344ea3a6dc4714
Big Sparrow <1109952026@qq.com>
2024-06-27 09:44:16 +0800

    Create constant.ts

---
+++ constant.ts
@@ -0,0 +1,20 @@
+export const WINDOW_OBJ_NAME = '__client_api__';
+
+export const QUERY_KEY = 'web_query';
+
+export const METHODS = {
+  EVENT: {
+    ADD_EVENT_LISTENER: 'event.addEventListener',
+    REMOVE_EVENT_LISTENER: 'event.removeEventListener',
+  },
+  API: {
+    CHECK: 'api.check',
+    LIST: 'api.list',
+  },
+  CLIENT: {
+    GET_CLIENT_INFO: 'client.getClientInfo',
+  },
+};
+
+export const EVENTS = {
+};
